04ea8d30aa800fe51fb2b9b647d87ab4c9db26f9
sadbinsiddique <mdsiyam377@gmail.com>
2026-06-11 15:15:39 +0600

    partial Update for single Variable

--- src/main/java/com/market/finder/rest/EmployeeController.java
+++ src/main/java/com/market/finder/rest/EmployeeController.java
@@ -3,17 +3,22 @@ package com.market.finder.rest;
 import com.market.finder.entity.Employee;
 import com.market.finder.service.EmployeeService;
 import org.springframework.web.bind.annotation.*;
+import tools.jackson.databind.json.JsonMapper;
 
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api")
 public class EmployeeController {
 
     private  final EmployeeService employeeService;
+    private final JsonMapper jsonMapper;
 
-    public EmployeeController(EmployeeService  injection) {
+
+    public EmployeeController(EmployeeService  injection, JsonMapper theJsonMapper) {
        this. employeeService = injection;
+       this.jsonMapper = theJsonMapper;
     }
 
     @GetMapping("/employee")  // it is get method
@@ -27,10 +32,40 @@ public class EmployeeController {
         return employeeService.save(theEmployee);
     }
 
-    //update methode
     @PutMapping("/employee")
     public Employee updateEmployee(@RequestBody Employee theEmployee) {
         return employeeService.save(theEmployee);
+    }
+
+    @PatchMapping("/employee/{employeeId}") // partial Update for employee
+    public Employee patchEmployee (@PathVariable int employeeId, @RequestBody Map<String, Object> patchPaylode) {
+        Employee tempEmployee = employeeService.findById(employeeId);
+
+        //throw exception if value Is null
+        if (tempEmployee == null) {
+            throw new RuntimeException("Employee is not found - "+ employeeId );
+        }
+
+        //throw exception if request body contain "id" key
+        if (patchPaylode.containsKey("id")) {
+            throw new RuntimeException("Employee id in not Allowed - " + employeeId );
+        }
+
+        Employee patchEmployee = jsonMapper.updateValue(tempEmployee, patchPaylode);
+
+        return employeeService.save(patchEmployee);
+    }
+
+    //add mapping for DELETE /employee/{employeeId}
+    @DeleteMapping("/employee/{employeeId}")
+    public String deleteEmployee(@PathVariable int employeeId) {
+        Employee tempEmployee = employeeService.findById(employeeId);
+
+        if (tempEmployee == null) {
+            throw new RuntimeException("Employee id not found - " + employeeId);
+        }
+        employeeService.deleteById(employeeId);
+        return "Delete Employee Id - " + employeeId;
     }
 
 }
