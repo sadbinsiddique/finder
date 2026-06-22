052e7749315691e99bccc870f188aa95ab3c56f1
sadbinsiddique <mdsiyam377@gmail.com>
2026-06-22 14:44:17 +0600

    no need to it
    Manual Code To Auto Code By (Spring RESR)
    
    Signed-off-by: sadbinsiddique <mdsiyam377@gmail.com>

--- src/main/java/com/market/finder/dao/EmployeeRepository.java
+++ src/main/java/com/market/finder/dao/EmployeeRepository.java
@@ -2,7 +2,9 @@ package com.market.finder.dao;
 
 import com.market.finder.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource(path = "members")
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
-    // there is No need to code
+    // No Need To Manual Code
 }
