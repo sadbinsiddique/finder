355d0be77ff29d6a65d7547d43829a99390daad6
sadbinsiddique <mdsiyam377@gmail.com>
2026-06-23 17:12:13 +0600

    Implement Role Base Entry (EMPLOYEE, MANAGER, ADMIN) fix
    Signed-off-by: sadbinsiddique <mdsiyam377@gmail.com>

--- src/main/java/com/market/finder/security/SecurityConfig.java
+++ src/main/java/com/market/finder/security/SecurityConfig.java
@@ -6,34 +6,18 @@ import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class SecurityConfig {
-
-    // Define in-memory users with roles
+    //add support for JDBC  .... no mere hardcoded users :)
     @Bean
-    public InMemoryUserDetailsManager userDetailManager() {
-        UserDetails john = User.builder()
-                .username("john")
-                .password("{noop}1234") // {noop} means plain text password (fine for testing)
-                .roles("EMPLOYEE")
-                .build();
-        UserDetails mary = User.builder()
-                .username("mary")
-                .password("{noop}1234")
-                .roles("EMPLOYEE", "MANAGER")
-                .build();
-        UserDetails susan = User.builder()
-                .username("susan")
-                .password("{noop}1234")
-                .roles("EMPLOYEE", "MANAGER", "ADMIN")
-                .build();
-
-        return new InMemoryUserDetailsManager(john, mary, susan);
+    public UserDetailsManager userDetailsManager(DataSource dataSource) {
+        return new JdbcUserDetailsManager(dataSource);
     }
 
     // Restrict URL access based on Roles
@@ -57,4 +41,27 @@ public class SecurityConfig {
 
         return http.build();
     }
+    /*
+    // Define in-memory users with roles
+    @Bean
+    public InMemoryUserDetailsManager userDetailManager() {
+        UserDetails john = User.builder()
+                .username("john")
+                .password("{noop}1234") // {noop} means plain text password (fine for testing)
+                .roles("EMPLOYEE")
+                .build();
+        UserDetails mary = User.builder()
+                .username("mary")
+                .password("{noop}1234")
+                .roles("EMPLOYEE", "MANAGER")
+                .build();
+        UserDetails susan = User.builder()
+                .username("susan")
+                .password("{noop}1234")
+                .roles("EMPLOYEE", "MANAGER", "ADMIN")
+                .build();
+
+        return new InMemoryUserDetailsManager(john, mary, susan);
+    }
+*/
 }
