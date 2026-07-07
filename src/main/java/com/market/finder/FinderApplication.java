b8bd2f201944346f431384679e2f53a6b8026bde
sadbinsiddique <mdsiyam377@gmail.com>
2026-07-07 16:28:38 +0600

    make Command Line Runner for run database (1nf, 2nf & 3nf)
    Signed-off-by: sadbinsiddique <mdsiyam377@gmail.com>

--- src/main/java/com/market/finder/FinderApplication.java
+++ src/main/java/com/market/finder/FinderApplication.java
@@ -1,13 +1,24 @@
 package com.market.finder;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
-@SpringBootApplication(scanBasePackages = "com.market.finder")
+@SpringBootApplication
 public class FinderApplication {
-
     static void main(String[] args) {
         SpringApplication.run(FinderApplication.class, args);
     }
 
+    @Bean
+    public CommandLineRunner cmdRunner() {
+        return args -> {
+            System.out.println("Custom Code Is Running.");
+        };
+    }
+
 }
+
+
+
