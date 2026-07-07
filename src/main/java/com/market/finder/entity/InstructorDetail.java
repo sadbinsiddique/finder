42106d5677efb34c8aba469632d39b53a82af8e3
sadbinsiddique <mdsiyam377@gmail.com>
2026-07-07 16:26:31 +0600

    entity if Instructor Detail
    Signed-off-by: sadbinsiddique <mdsiyam377@gmail.com>

---
+++ src/main/java/com/market/finder/entity/InstructorDetail.java
@@ -0,0 +1,57 @@
+package com.market.finder.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "instructor_detail")
+public class InstructorDetail {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+    @Column(name = "age")
+    private int age;
+    @Column(name = "hobby")
+    private String hobby;
+
+    public InstructorDetail() {
+    }
+
+    public InstructorDetail(int age, String hobby) {
+        this.age = age;
+        this.hobby = hobby;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getHobby() {
+        return hobby;
+    }
+
+    public void setHobby(String hobby) {
+        this.hobby = hobby;
+    }
+
+    @Override
+    public String toString() {
+        return "InstructorDetail{" +
+                "id=" + id +
+                ", age=" + age +
+                ", hobby='" + hobby + '\'' +
+                '}';
+    }
+}
