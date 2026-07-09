69739e361beb58afbcbfd6dcc234bc3b8918ffba
sadbinsiddique <mdsiyam377@gmail.com>
2026-07-09 13:41:33 +0600

    add Find interface
    Signed-off-by: sadbinsiddique <mdsiyam377@gmail.com>

--- src/main/java/com/market/finder/dao/AppDAO.java
+++ src/main/java/com/market/finder/dao/AppDAO.java
@@ -1,7 +1,12 @@
 package com.market.finder.dao;
 
 import com.market.finder.entity.Instructor;
+import com.market.finder.entity.InstructorDetail;
 
 public interface AppDAO {
     void save(Instructor theInstructor);
+
+    Instructor findInstructorById(int theId);
+
+    InstructorDetail findInstructorDetailById(int theId);
 }
