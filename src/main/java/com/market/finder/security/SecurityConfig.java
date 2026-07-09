cc44323c514695ac125ef9cce28085af12386aaf
sadbinsiddique <mdsiyam377@gmail.com>
2026-07-09 12:59:55 +0600

    I have disabled password security for temporary
    Signed-off-by: sadbinsiddique <mdsiyam377@gmail.com>

--- src/main/java/com/market/finder/security/SecurityConfig.java
+++ src/main/java/com/market/finder/security/SecurityConfig.java
@@ -2,8 +2,6 @@ package com.market.finder.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
@@ -24,6 +22,8 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests(configurer ->
                 configurer
+                        .anyRequest().permitAll()
+                        /*
                         .requestMatchers("/swagger",
                                 "/swagger-ui/**",
                                 "/v3/api-docs/**").permitAll()
@@ -36,8 +36,9 @@ public class SecurityConfig {
                         .requestMatchers(HttpMethod.PATCH, "/api/employees/**").hasRole("MANAGER")
                         // --- Everything else requires authentication ---
                         .anyRequest().authenticated()
+                        */
         );
-        http.httpBasic(Customizer.withDefaults());
+        // http.httpBasic(Customizer.withDefaults());
         http.csrf(AbstractHttpConfigurer::disable);
         return http.build();
     }
