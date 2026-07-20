b6306f4ce6858c834c2792a3304e5985a73d28d8
sadbinsiddique <mdsiyam377@gmail.com>
2026-07-20 15:37:22 +0600

    Refactor Instructor entity with Lombok and add user and course relationships
    Signed-off-by: sadbinsiddique <mdsiyam377@gmail.com>

--- src/main/java/com/market/finder/entity/Instructor.java
+++ src/main/java/com/market/finder/entity/Instructor.java
@@ -1,81 +1,44 @@
 package com.market.finder.entity;
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+import java.util.Set;
+
+@Getter
+@Setter
+@NoArgsConstructor
 @Entity
 @Table(name = "instructor")
 public class Instructor {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id")
-    private int id;
-    @Column(name = "first_name")
+    private Integer id;
+
+    @Column(name = "first_name", length = 45)
     private String firstName;
-    @Column(name = "last_name")
+
+    @Column(name = "last_name", length = 45)
     private String lastName;
-    @Column(name = "email")
+
+    @Column(length = 64)
     private String email;
+
     @OneToOne(cascade = CascadeType.ALL)
     @JoinColumn(name = "instructor_detail_id")
     private InstructorDetail instructorDetail;
 
-    public Instructor() {
-    }
+    @OneToOne
+    @JoinColumn(name = "username", referencedColumnName = "username", unique = true)
+    private User user;
 
-    public Instructor(String email, String firstName, String lastName) {
-        this.email = email;
-        this.firstName = firstName;
-        this.lastName = lastName;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public InstructorDetail getInstructorDetail() {
-        return instructorDetail;
-    }
-
-    public void setInstructorDetail(InstructorDetail instructorDetail) {
-        this.instructorDetail = instructorDetail;
-    }
-
-    @Override
-    public String toString() {
-        return "Instructor{" +
-                "email='" + email + '\'' +
-                ", id=" + id +
-                ", firstName='" + firstName + '\'' +
-                ", lastName='" + lastName + '\'' +
-                ", instructorDetail=" + instructorDetail +
-                '}';
-    }
+    @ManyToMany
+    @JoinTable(
+            name = "teaching_assignment",
+            joinColumns = @JoinColumn(name = "instructor_id"),
+            inverseJoinColumns = @JoinColumn(name = "course_id")
+    )
+    private Set<Course> courses;
 }
