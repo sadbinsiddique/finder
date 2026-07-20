038ec07a9d0ed478ac10afc93c68c42b729de722
sadbinsiddique <mdsiyam377@gmail.com>
2026-07-20 15:36:45 +0600

    Refactor InstructorDetail entity with Lombok
    Signed-off-by: sadbinsiddique <mdsiyam377@gmail.com>

--- src/main/java/com/market/finder/entity/InstructorDetail.java
+++ src/main/java/com/market/finder/entity/InstructorDetail.java
@@ -1,80 +1,22 @@
 package com.market.finder.entity;
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@Getter
+@Setter
+@NoArgsConstructor
 @Entity
 @Table(name = "instructor_detail")
 public class InstructorDetail {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id")
-    private int id;
-    @Column(name = "age")
-    private int age;
-    @Column(name = "youtube_channel")
+    private Integer id;
+
+    private Integer age;
+
+    @Column(name = "youtube_channel", length = 128)
     private String youtubeChannel;
-
-    //for bidirectional connection
-    //++++++++++++++++++++++++++++
-    //@OneToOne(mappedBy = "instructorDetail", cascade = CascadeType.ALL)
-    @OneToOne(mappedBy = "instructorDetail", cascade = {CascadeType.DETACH, CascadeType.MERGE, CascadeType.PERSIST,
-            CascadeType.REFRESH})
-    private Instructor instructor;
-
-    public Instructor getInstructor() {
-        return instructor;
-    }
-
-    public void setInstructor(Instructor instructor) {
-        this.instructor = instructor;
-    }
-
-    //+++++++++++++++++++++++++++
-
-    public InstructorDetail() {
-    }
-
-    public InstructorDetail(int age, String youtubeChannel) {
-        this.age = age;
-        this.youtubeChannel = youtubeChannel;
-    }
-
-    public String getYoutubeChannel() {
-        return youtubeChannel;
-    }
-
-    public void setYoutubeChannel(String youtubeChannel) {
-        this.youtubeChannel = youtubeChannel;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
-
-    public void setInstructorDetail(InstructorDetail tempInstructorDetail) {
-        tempInstructorDetail.age = age;
-        tempInstructorDetail.youtubeChannel = youtubeChannel;
-
-    }
-
-    @Override
-    public String toString() {
-        return "InstructorDetail{" +
-                "id=" + id +
-                ", age=" + age +
-                ", youtubeChannel='" + youtubeChannel + '\'' +
-                '}';
-    }
 }
