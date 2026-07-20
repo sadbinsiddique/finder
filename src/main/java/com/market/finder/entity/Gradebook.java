998c81e9146659424359b0ac3d4be1f98b05a70f
sadbinsiddique <mdsiyam377@gmail.com>
2026-07-20 15:40:23 +0600

    Add Gradebook entity with Lombok and student and course relationships
    Signed-off-by: sadbinsiddique <mdsiyam377@gmail.com>

---
+++ src/main/java/com/market/finder/entity/Gradebook.java
@@ -0,0 +1,65 @@
+package com.market.finder.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.util.Objects;
+
+@Embeddable
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+class GradebookId implements Serializable {
+    @Column(name = "student_id")
+    private Integer studentId;
+
+    @Column(name = "course_id")
+    private Integer courseId;
+
+    @Column(name = "assignment_name", length = 100)
+    private String assignmentName;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        GradebookId that = (GradebookId) o;
+        return Objects.equals(studentId, that.studentId) &&
+                Objects.equals(courseId, that.courseId) &&
+                Objects.equals(assignmentName, that.assignmentName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(studentId, courseId, assignmentName);
+    }
+}
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+@Table(name = "gradebook")
+public class Gradebook {
+    @EmbeddedId
+    private GradebookId id = new GradebookId();
+
+    @ManyToOne
+    @MapsId("studentId")
+    @JoinColumn(name = "student_id")
+    private Student student;
+
+    @ManyToOne
+    @MapsId("courseId")
+    @JoinColumn(name = "course_id")
+    private Course course;
+
+    @Column(precision = 5, scale = 2)
+    private BigDecimal score;
+}
