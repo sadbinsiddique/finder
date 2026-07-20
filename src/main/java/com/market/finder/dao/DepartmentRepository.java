801b1efcaf2f0ff9a850a53bd45b1911920b17a0
sadbinsiddique <mdsiyam377@gmail.com>
2026-07-20 15:59:18 +0600

    Add DepartmentRepository interface
    Signed-off-by: sadbinsiddique <mdsiyam377@gmail.com>

---
+++ src/main/java/com/market/finder/dao/DepartmentRepository.java
@@ -0,0 +1,12 @@
+package com.market.finder.dao;
+
+import com.market.finder.entity.Department;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface DepartmentRepository extends JpaRepository<Department, Integer> {
+    Optional<Department> findByName(String name);
+}
