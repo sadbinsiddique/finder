1222b3e71d7a67e974772a2363d725283ac877d5
sadbinsiddique <mdsiyam377@gmail.com>
2026-07-20 15:58:22 +0600

    Add PermissionRepository interface
    Signed-off-by: sadbinsiddique <mdsiyam377@gmail.com>

---
+++ src/main/java/com/market/finder/dao/PermissionRepository.java
@@ -0,0 +1,12 @@
+package com.market.finder.dao;
+
+import com.market.finder.entity.Permission;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface PermissionRepository extends JpaRepository<Permission, Integer> {
+    Optional<Permission> findByPermissionName(String permissionName);
+}
