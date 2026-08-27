99ca7e1d15392d5425fa833f5113af88f245db60
sadbinsiddique <mdsiyam377@gmail.com>
2026-08-27 13:46:19 +0600

    add manual Validation in reg controller

--- src/main/java/com/market/finder/dto/RegisterRequest.java
+++ src/main/java/com/market/finder/dto/RegisterRequest.java
@@ -1,5 +1,4 @@
 package com.market.finder.dto;
-import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
