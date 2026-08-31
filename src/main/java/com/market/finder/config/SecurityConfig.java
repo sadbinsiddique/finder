e421b4ec6d602855d5b79797a9b11aa31c42e54b
sadbinsiddique <mdsiyam377@gmail.com>
2026-09-01 02:28:59 +0600

    fix common entity

--- src/main/java/com/market/finder/config/SecurityConfig.java
+++ src/main/java/com/market/finder/config/SecurityConfig.java
@@ -40,7 +40,7 @@ public class SecurityConfig {
     }
 
     @Bean
-    public AuthenticationManager authenticationManager(AuthenticationConfiguration authConfig) throws Exception {
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authConfig) {
         return authConfig.getAuthenticationManager();
     }
 
@@ -55,7 +55,7 @@ public class SecurityConfig {
     }
 
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http, PasswordEncoder passwordEncoder) throws Exception {
+    public SecurityFilterChain filterChain(HttpSecurity http, PasswordEncoder passwordEncoder) {
         http.authenticationProvider(authenticationProvider(passwordEncoder));
 
         http.authorizeHttpRequests(configurer ->
